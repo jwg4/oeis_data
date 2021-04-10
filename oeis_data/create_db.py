21ac6b7a91cfba59e8e66577b40d983b9849f7c6
Jack Grahl <jack.grahl@gmail.com>
2021-04-10 01:25:24 +0100

    Try to write data to the db.

--- oeis_data/create_db.py
+++ oeis_data/create_db.py
@@ -1,2 +1,15 @@
+import csv
+
+import sqlite_utils
+
+
+def _run(db_name, data_source):
+    db = sqlite_utils.Database(db_name)
+    with open(data_source) as f: 
+        db["sequences"].insert_all(
+            csv.DictReader(f)
+        )
+
+
 def run():
-    pass
+    _run("data/sequences.db", "raw/stripped")
